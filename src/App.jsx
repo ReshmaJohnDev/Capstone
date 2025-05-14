67230aca5adf17ef13e404906b42f3424b3aa6a7
Reshma John <reshma.chackola@gmail.com>
2025-05-14 11:47:12 +0200

    Add in the incorrect guesses mechanism to the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
 import { useState } from "react";
+import { clsx } from "clsx";
 import { languages } from "./languages";
-import clsx from "clsx";
+
+/**
+ * Goal: Add in the incorrect guesses mechanism to the game
+ *
+ * Challenge:
+ * Conditionally render either the "won" or "lost" statuses
+ * from the design, both the text and the styles, based on the
+ * new derived variables.
+ *
+ * Note: We always want the surrounding `section` to be rendered,
+ * so only change the content inside that section. Otherwise the
+ * content on the page would jump around a bit too much.
+ */
 
 export default function AssemblyEndgame() {
+  // State values
   const [currentWord, setCurrentWord] = useState("react");
   const [guessedLetters, setGuessedLetters] = useState([]);
 
+  // Derived values
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.includes(letter)
+  ).length;
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+  const isGameOver = isGameWon || isGameLost;
+
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+  });
+
+  // Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   function addGuessedLetter(letter) {
-    console.log("inside fn");
     setGuessedLetters((prevLetters) =>
       prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
     );
   }
 
-  const languageElements = languages.map((lang) => {
+  const languageElements = languages.map((lang, index) => {
+    const isLanguageLost = index < wrongGuessCount;
     const styles = {
       backgroundColor: lang.backgroundColor,
       color: lang.color,
     };
+    const className = clsx("chip", isLanguageLost && "lost");
     return (
-      <span className="chip" style={styles} key={lang.name}>
+      <span className={className} style={styles} key={lang.name}>
         {lang.name}
       </span>
     );
@@ -29,21 +60,25 @@ export default function AssemblyEndgame() {
 
   const letterElements = currentWord
     .split("")
-    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+    .map((letter, index) => (
+      <span key={index}>
+        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+      </span>
+    ));
 
   const keyboardElements = alphabet.split("").map((letter) => {
     const isGuessed = guessedLetters.includes(letter);
     const isCorrect = isGuessed && currentWord.includes(letter);
     const isWrong = isGuessed && !currentWord.includes(letter);
-
-    const buttonClass = clsx({
-      on: isCorrect,
-      off: isWrong,
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
     });
+
     return (
       <button
+        className={className}
         key={letter}
-        className={buttonClass}
         onClick={() => addGuessedLetter(letter)}
       >
         {letter.toUpperCase()}
@@ -60,14 +95,30 @@ export default function AssemblyEndgame() {
           from Assembly!
         </p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+
+      <section className={gameStatusClass}>
+        {isGameOver ? (
+          isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ) : (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : null}
       </section>
+
       <section className="language-chips">{languageElements}</section>
+
       <section className="word">{letterElements}</section>
+
       <section className="keyboard">{keyboardElements}</section>
-      <button className="new-game">New Game</button>
+
+      {isGameOver && <button className="new-game">New Game</button>}
     </main>
   );
 }
