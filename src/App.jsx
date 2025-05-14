a515db12f31d451d37a6b57da9850920665eb8ab
Reshma John <reshma.chackola@gmail.com>
2025-05-14 06:17:46 +0200

    Added clsx class feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,56 @@
-import React from "react";
-import { languages } from "./languages";
 import { useState } from "react";
-
-/**
- * Goal: Build out the main parts of our app
- *
- * Challenge:
- * 1. Save a "currentWord" in state. Initialize as "react".
- * 2. Map over the letters of the word (you'll need to turn
- *    the string into an array of letters first) and display
- *    each one as a <span>. Capitalize the letters when
- *    displaying them.
- * 3. Style to look like the design. You can get the underline
- *    effect on the box using `border-bottom`.
- */
+import { languages } from "./languages";
+import clsx from "clsx";
 
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react");
-  const alphabets = "abcdefghijklmnopqrstuvwxyz";
-  const keyboardElement = alphabets.split("").map((alphabet, index) => {
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuessedLetter(letter) {
+    console.log("inside fn");
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  }
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
     return (
-      <button key={index} className="key">
-        {alphabet.toUpperCase()}
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+
+    const buttonClass = clsx({
+      on: isCorrect,
+      off: isWrong,
+    });
+    return (
+      <button
+        key={letter}
+        className={buttonClass}
+        onClick={() => addGuessedLetter(letter)}
+      >
+        {letter.toUpperCase()}
       </button>
     );
   });
 
-  const guessWord = currentWord.split("").map((letter, index) => {
-    return (
-      <span key={index} className="guess-letter">
-        {letter.toUpperCase()}
-      </span>
-    );
-  });
-
-  const languageElements = languages.map((chip) => {
-    const style = {
-      backgroundColor: chip.backgroundColor,
-      color: chip.color,
-    };
-    return (
-      <span style={style} className="chip">
-        {chip.name}
-      </span>
-    );
-  });
   return (
     <main>
       <header>
@@ -58,9 +64,10 @@ export default function AssemblyEndgame() {
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
       </section>
-      <section className="language_chip">{languageElements}</section>
-      <section className="guess-word">{guessWord}</section>
-      <section className="keyboard">{keyboardElement}</section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   );
 }
